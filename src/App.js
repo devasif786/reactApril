472f92aa27a0dfbb6bc146885eb974ba874c1921
devasif786 <webdevasif786@gmail.com>
2023-04-26 19:41:28 +0530

    name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Counter from './Component/Counter';
 
 function App() {
 
@@ -9,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-   <Counter/>
+  
     </div>
   );
 }
