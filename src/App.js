cbf280067315f01859db06cb312059a3bd09c872
devasif786 <webdevasif786@gmail.com>
2023-04-26 23:53:35 +0530

    NameUpdater

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import NameUpdater from './Component/NameUpdater';
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-  
+  <NameUpdater/>
     </div>
   );
 }
