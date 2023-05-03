a2886bab567da3a18178400861b18e11eb81de39
devasif786 <webdevasif786@gmail.com>
2023-05-03 09:41:54 +0530

    FextApi 1st question

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import './App.css';
-import { Button } from './components/button/button';
-import Card from './components/card/card';
+import React,{useState,useEffect} from "react";
 
 function App() {
 
-  //  components -/> small isolated piece of code of you complete ui .
-  // components  -/> functions (react function)
-  //  js function -/> takes datatype as input and return data types
-  // react function -/> takes props as input and return jsx / html likw components .
-  //  react functions startWith Capital letter only . 
+const [data , setData]=useState([])
+  useEffect (()=>{
+    async function data(){
+    const response= await fetch("https://dog.ceo/api/breeds/image/random")
+    const jason=await response.json()
+    console.log("hello")
+    console.log(jason)
+    setData(jason)
 
+   
+    }
+    data();
+  },[])
 
-//   Jsx -/> javascript extension 
-//   it provides the power to write the html in js file 
-
-//  ui 
-//  functionality 
-//  data management 
-//  folder structure (Atomic folder structure)
-
-
-//  css  -> style to style the component
-//  js -/> code to ceate the component 
 
 
 
   return (
     <div className="App">
-      <h1>Welcome to react js  ....</h1>
-      <Button/>
-      <Card/>
+<img src={data.message}/>
+  
     </div>
   );
 }
