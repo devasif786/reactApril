c9fc83d4c8b1c1722e3e15e5db930e9b236d63aa
devasif786 <webdevasif786@gmail.com>
2023-05-04 12:38:43 +0530

    otp generate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React,{useState,useEffect} from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [mobile, setMobile] = useState('');
+  const [otpSent, setOtpSent] = useState(false);
+  const [error, setError] = useState(null);
 
-const [data , setData]=useState([])
-  useEffect (()=>{
-    async function data(){
-    const response= await fetch("https://dog.ceo/api/breeds/image/random")
-    const jason=await response.json()
-    console.log("hello")
-    console.log(jason)
-    setData(jason)
+  const handleMobileChange = (event) => {
+    setMobile(event.target.value);
+  };
 
-   
+  const handleSendOtp = (event) => {
+    event.preventDefault();
+    if (/^[6-9]\d{9}$/.test(mobile)) {
+      fetch("https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ mobile: parseInt(mobile) }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            setOtpSent(true);
+            setError(null);
+            setTimeout(() => {
+              setOtpSent(false);
+            }, 120000);
+          } else {
+            setError('Failed to send OTP. Please try again later.');
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          setError('Failed to send OTP. Please check your network connection and try again.');
+        });
+    } else {
+      setError('Please enter a valid Indian mobile number.');
     }
-    data();
-  },[])
-
-
-
+  };
 
   return (
     <div className="App">
-<img src={data.message}/>
-  
+      <form onSubmit={handleSendOtp}>
+        <label htmlFor="mobile">Mobile number:</label>
+        <input
+          type="tel"
+          id="mobile"
+          name="mobile"
+          pattern="[6-9]\d{9}"
+          value={mobile}
+          onChange={handleMobileChange}
+          required
+        />
+        <button type="submit">Send OTP</button>
+      </form>
+      {otpSent && <p>OTP sent successfully. Please check your mobile.</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
 }
