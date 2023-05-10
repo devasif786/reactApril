63e79bbb8692148e2f0beef476d14baa9e6fc185
devasif786 <webdevasif786@gmail.com>
2023-05-11 01:13:48 +0530

    ScrollBar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React,{useState,useEffect} from "react";
+import React from "react";
+import ScrollBar from "./Component/Molicule/ScrollBar";
+
 
 function App() {
 
-const [data , setData]=useState([])
-  useEffect (()=>{
-    async function data(){
-    const response= await fetch("https://dog.ceo/api/breeds/image/random")
-    const jason=await response.json()
-    console.log("hello")
-    console.log(jason)
-    setData(jason)
 
-   
-    }
-    data();
-  },[])
 
 
 
 
   return (
     <div className="App">
-<img src={data.message}/>
+<ScrollBar/>
   
     </div>
   );
---
+++ src/Component/Molicule/ScrollBar.js
@@ -0,0 +1,37 @@
+import React,{useState,useEffect} from 'react'
+import Card from '../atom/Card'
+import style1 from './ScrollBar.module.css'
+
+export default function ScrollBar
+() {
+    const [data , setData]=useState([])
+  useEffect (()=>{
+    async function data(){
+    const response= await fetch("https://reqres.in/api/users/")
+    const jason=await response.json()
+    console.log("hello")
+    console.log(jason.data)
+    setData(jason.data)
+
+   
+    }
+    data();
+  },[])
+  return (
+    <div className={style1.wrapper}>
+        {
+            data.map((ele)=>(
+                <Card key={ele.id} item={ele}/>
+            ))
+          
+        }
+         {
+            data.map((ele)=>(
+                <Card key={ele.id} item={ele}/>
+            ))
+          
+        }
+        
+    </div>
+  )
+}
