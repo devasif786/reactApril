6b16eee66b2f79e232bdc8c77669085d074df1d2
devasif786 <webdevasif786@gmail.com>
2023-05-20 00:15:17 +0530

    QUIZAPP

--- src/COMPONENTS/ATOM/MOLICULE/Quiz.js
+++ src/COMPONENTS/ATOM/MOLICULE/Quiz.js
@@ -1,104 +1,104 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './Quiz.module.css'
 import { QuizData } from '../../../Data/Data'
 import Quizresult from './Quizresult'
 export default function Quiz() {
-    const [currentQuestion,setcurrentQuestion]=useState(0)
-    const[score,setScore]=useState(0)
-    const[clickOption,setclickoption]=useState(0)
-    const [show,setShow]=useState(false)
-    const [btnSubmit,setbtnSubmit]=useState(true)
+    const [currentQuestion, setcurrentQuestion] = useState(0)
+    const [score, setScore] = useState(0)
+    const [clickOption, setclickoption] = useState(0)
+    const [show, setShow] = useState(false)
+    const [btnSubmit, setbtnSubmit] = useState(true)
     const [timer, setTimer] = useState(600);
 
+
     useEffect(() => {
-        
-      const interval = setInterval(() => {
-        setTimer((prevTimer) => prevTimer - 1);
-      }, 1000);
-  
-      return () => {
-        clearInterval(interval);
-      };
+
+        const interval = setInterval(() => {
+            setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
             changeQuestion()
         }, 60000)
-    
+
         return () => clearInterval(interval)
-    },[currentQuestion] )
-   
-  
- 
-   function changeQuestion(){
- 
+    }, [currentQuestion])
 
-    updateScore();
 
-    if(currentQuestion==QuizData.length-2){
-        setbtnSubmit(false)
-    } 
-    if(currentQuestion<QuizData.length-1){
-    setcurrentQuestion(currentQuestion+1)
-     setclickoption(0)
-    
-    
-    }else{
-       setShow(true)
-    
+// function for change Question 
+    function changeQuestion() {
+      updateScore();
+
+        if (currentQuestion == QuizData.length - 2) {
+            setbtnSubmit(false)
+        }
+        if (currentQuestion < QuizData.length - 1) {
+            setcurrentQuestion(currentQuestion + 1)
+            setclickoption(0)
+
+
+        } else {
+            setShow(true)
+
+        }
+
+    }
+    //update score
+    function updateScore() {
+        if (clickOption === QuizData[currentQuestion].answer) {
+            setScore(score + 2)
+
+        }
+
     }
 
-   }
-   function updateScore(){
-       if(clickOption===QuizData[currentQuestion].answer){
-        setScore(score+2)
-       
-       }
-    
-   }
+//reset all
+    function reset() {
+        setShow(false)
+        setcurrentQuestion(0)
+        setclickoption(0)
+        setScore(0)
+        setbtnSubmit(true)
+        setTimer(600)
+    }
 
-   
-   function reset(){
-    setShow(false)
-    setcurrentQuestion(0)
-    setclickoption(0)
-    setScore(0)
-    setbtnSubmit(true)
-    setTimer(600)
-   }
-   
-  return (
+    return (
 
 
-    <div>
+        <div>
 
-   <h1 className={style.heading}>QUIZ APP  <h1></h1></h1> 
-   
-   <div className={style.container}>
-    {show? (
-        <Quizresult score={score} totalScore={QuizData.length*2} tryAgain={reset} count={currentQuestion}/>
-    ):(
-        <>
-    <div className={style.question}>
-        <span id={style.question}>{currentQuestion+1}.</span>
-        <span id={style.qtext}>{QuizData[currentQuestion].question}</span>
-    </div>
-    <div className={style.optionContainer}>
-        {QuizData[currentQuestion].options.map((options,i)=>{
-            return (<button className={`option-btn ${clickOption==i+1?"checked":null}`}
-                key={i}
-            onClick={()=>setclickoption(i+1)}
-            type='radio'
-            >{options}</button>)
-        })}
-    </div>
-    {btnSubmit?(<><input type="button" value="Next" onClick={changeQuestion} className={style.button}/></>):(<><button onClick={changeQuestion} className={style.button}>Submit</button></>)}
-   <h1> Second: {timer} </h1> 
+            <h1 className={style.heading}>QUIZ APP  <h1></h1></h1>
 
-   </>)}
-   </div>
+            <div className={style.container}>
+                {show ? (
+                    <Quizresult score={score} totalScore={QuizData.length * 2} tryAgain={reset}  />
+                ) : (
+                    <>
+                        <div className={style.question}>
+                            <span id={style.question}>{currentQuestion + 1}.</span>
+                            <span id={style.qtext}>{QuizData[currentQuestion].question}</span>
+                        </div>
+                        <div className={style.optionContainer}>
+                            {QuizData[currentQuestion].options.map((options, i) => {
+                                return (<button type='radio' className={`option-btn ${clickOption == i +1 ? "checked" : null}`}
+                                    key={i}
+                                    onClick={() => setclickoption(i + 1)}
+                                    
+                                >{options}</button>)
+                            })}
+                        </div>
+                        {btnSubmit ? (<><input type="button" value="Next" onClick={changeQuestion} className={style.button} /></>) : (<><button onClick={changeQuestion} className={style.button}>Submit</button></>)}
+                        <h1> Second: {timer} </h1>
 
-    </div>
-  )
+                    </>)}
+            </div>
+
+        </div>
+    )
 }
--- src/COMPONENTS/ATOM/MOLICULE/Quizresult.js
+++ src/COMPONENTS/ATOM/MOLICULE/Quizresult.js
@@ -3,10 +3,11 @@ import React, {  useState } from 'react'
 export default function Quizresult(props) {
  
   return (
-    <div>Quizresult
+    <div style={{textAlign:"center"}}>Quizresult
    {props.score>11?(<><h1 >Congratulation you are Pass</h1><div>your Score   {props.score}</div> 
     <div> total Score:{props.totalScore}</div>
    <button className="option-btn" onClick={props.tryAgain}>Try Again</button></>):(<>
+   <h1>Sorry you are failed</h1>
     <div>your Score   {props.score}</div> 
     <div> total Score:{props.totalScore}</div>
    <button className="option-btn" onClick={props.tryAgain}>Try Again</button>
