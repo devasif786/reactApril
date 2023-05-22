0237cc9c43fb130dd38f7d1444a840b57198648e
devasif786 <webdevasif786@gmail.com>
2023-05-22 18:20:53 +0530

    MAin

--- src/App.js
+++ src/App.js
@@ -2,27 +2,12 @@ import React,{useState,useEffect} from "react";
 
 function App() {
 
-const [data , setData]=useState([])
-  useEffect (()=>{
-    async function data(){
-    const response= await fetch("https://dog.ceo/api/breeds/image/random")
-    const jason=await response.json()
-    console.log("hello")
-    console.log(jason)
-    setData(jason)
-
-   
-    }
-    data();
-  },[])
-
 
 
 
   return (
     <div className="App">
-<img src={data.message}/>
-  
+      
     </div>
   );
 }
