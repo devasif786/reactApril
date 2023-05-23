214838fdfc479f897bf3bcf28df78f5f5ac833c8
devasif786 <webdevasif786@gmail.com>
2023-05-23 11:04:06 +0530

    AutoSearch

--- src/Component/Atom/Search.js
+++ src/Component/Atom/Search.js
@@ -9,7 +9,7 @@ export default function Search() {
     const[input,setInput]=useState("")
     const fetchData= (value)=>{
         fetch("http://cdn-api.co-vin.in/api/v2/admin/location/states").then((response)=>response.json()).then((json)=>{const result=json.states.filter((staName)=>{
-            return value && staName.state_name && staName.state_name.toLowerCase().includes(value)||staName.state_name.toUpperCase().includes(value)
+            return value && staName.state_name && staName.state_name.toLowerCase().includes(value)|| value && staName.state_name.toUpperCase().includes(value)
         })
         console.log(result)
        setData(result)
