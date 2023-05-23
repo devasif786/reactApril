154ad7778261d7aafe82301c8d08a986b2c39f03
devasif786 <webdevasif786@gmail.com>
2023-05-23 10:54:01 +0530

    AutoSearch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from "react";
-
+import Search from "./Component/Atom/Search";
+import './App.css'
 function App() {
 
 
@@ -7,7 +8,10 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div className="searchDiv">
+        <Search/>
+      </div>
+      <div></div>
     </div>
   );
 }
---
+++ src/Component/Atom/Search.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react'
+import axios from 'axios'
+import style from './Search.module.css'
+import {FaSearch} from 'react-icons/fa'
+import {BiMicrophone} from 'react-icons/bi'
+//BiMicrophone
+export default function Search() {
+    const[data, setData]=useState([])
+    const[input,setInput]=useState("")
+    const fetchData= (value)=>{
+        fetch("http://cdn-api.co-vin.in/api/v2/admin/location/states").then((response)=>response.json()).then((json)=>{const result=json.states.filter((staName)=>{
+            return value && staName.state_name && staName.state_name.toLowerCase().includes(value)
+        })
+        console.log(result)
+       setData(result)
+      })
+      
+    }
+   
+    function handleText(value){
+        setInput(value)
+        fetchData(value)
+    }
+    function handleData(value1){
+      setInput(value1)
+    }
+  return (
+    <>
+    <div className={style.inputWrapper}>
+   <FaSearch id={style.searchIcon}/>
+   <input type="text" placeholder='Type Your Text'
+   value={input} onChange={(e)=>handleText(e.target.value)}
+   />
+   <BiMicrophone id={style.searchIcon1}/>
+    </div>
+    <div className={style.Res}>
+{data.map((name,id)=>
+<h6 key={id} type="submit" onClick={(e)=>handleData(name.state_name)}>{name.state_name}</h6>)}
+
+    </div>
+    </>
+  )
+}
